f7fdc5e64b63c4cbb0ee206bc060f22b28ad5bdf
CODER-230907 <prathameshnc07@gmail.com>
2025-11-20 01:00:45 +0530

    Add files via upload

---
+++ app-fallback.js
@@ -0,0 +1,5 @@
+window.FALLBACK_ALL = [
+  {"id":"tree0001","name":"Neem","species":"Azadirachta indica","price":299,"stock":12,"type":"trees","tags":["medicinal","shade"],"image":"assets/img/neem.jpg","info":"Hardy tree, good for large gardens","vendor_id":"seller_demo_1"},
+  {"id":"indoor0001","name":"Snake Plant","species":"Sansevieria trifasciata","price":249,"stock":6,"type":"indoor","tags":["low-light","air-purifying"],"image":"assets/img/snake-plant.jpg","info":"Low maintenance indoor plant","vendor_id":"seller_demo_2"},
+  {"id":"herb0001","name":"Basil","species":"Ocimum basilicum","price":39,"stock":50,"type":"herbs","tags":["edible","fragrant"],"image":"assets/img/basil.jpg","info":"Great for kitchen gardens","vendor_id":"seller_demo_1"}
+];
